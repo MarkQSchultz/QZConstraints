3b7d1684e255d4474f092db6977a1a98d51597fe
Mark Schultz <mark.schultz@domo.com>
2014-06-12 22:07:09 -0600

    Implemented the actual methods to be used

--- classes/NSLayoutConstraint+QZConstraints.h
+++ classes/NSLayoutConstraint+QZConstraints.h
@@ -10,4 +10,55 @@
 
 @interface NSLayoutConstraint (QZConstraints)
 
+/*! Creates constraints that horizontally fills a view inside of its superview.  The subview will stretch from the left side of the superview to the right side.
+ * \param subview The subview that you want to fill horizontally inside of its superview
+ * \returns The constraints that horizontally fill the subview inside of its superview
+ */
++ (NSArray *)constraintsThatFillHorizontallyWithSubview:(UIView *)subview;
+
+/*! Creates constraints that vertically fills a view inside of its superview.  The subview will stretch from the top side of the superview to the bottom side.
+ * \param subview The subview that you want to fill vertically inside of its superview
+ * \returns The constraints that vertically fill the subview inside of its superview
+ */
++ (NSArray *)constraintsThatFillVerticallyWithSubview:(UIView *)subview;
+
+/*! Creates constraints that fills a view inside of its superview.  The subview will have the same edges as the superview.
+ * \param subview The subview that you want to fill inside of its superview
+ * \returns The constraints that fill the subview inside of its superview
+ */
++ (NSArray *)constraintsThatFillWithSubview:(UIView *)subview;
+
+/*! Creates constraints that centers a view inside of its superview.  The subview will be centered horizontally and vertically.
+ * \param subview The subview that you want to center inside of its superview
+ * \returns The constraints that center the subview inside of its superview
+ */
++ (NSArray *)constraintsThatCenterSubview:(UIView *)subview;
+
+/*! Creates a constraint that aligns two views by the given attribute.
+ * \param view One of the views that you want aligned
+ * \param otherView The other view that you want aligned
+ * \param attribute The attribute by which you want the two views aligned
+ * \returns The constraint that aligns the two views
+ */
++ (NSLayoutConstraint *)constraintAligningView:(UIView *)view withOtherView:(UIView *)otherView onAttribute:(NSLayoutAttribute)attribute;
+
+/*! Creates a constraint that aligns a subview with its superview by the given attribute
+ * \param subview The subview that you want aligned to its superview
+ * \param attribute The attribute by which you want to align the subview with its superview
+ * \returns The constraint that aligns the subview with its superview
+ */
++ (NSLayoutConstraint *)constraintAligningSubview:(UIView *)subview onAttribute:(NSLayoutAttribute)attribute;
+
+/*! Creates a constraint that centers a view horizontally inside of its superview.
+ * \param subview The subview that you want to center horizontally inside of its superview
+ * \returns The constraint that horizontally centers the subview inside of its superview
+ */
++ (NSLayoutConstraint *)constraintToHorizontallyCenterSubview:(UIView *)subview;
+
+/*! Creates a constraint that centers a view vertically inside of its superview.
+ * \param subview The subview that you want to center vertically inside of its superview
+ * \returns The constraint that vertically centers the subview inside of its superview
+ */
++ (NSLayoutConstraint *)constraintToVerticallyCenterSubview:(UIView *)subview;
+
 @end
--- classes/NSLayoutConstraint+QZConstraints.m
+++ classes/NSLayoutConstraint+QZConstraints.m
@@ -10,4 +10,68 @@
 
 @implementation NSLayoutConstraint (QZConstraints)
 
++ (NSArray *)constraintsThatFillHorizontallyWithSubview:(UIView *)subview
+{
+    return [NSLayoutConstraint constraintsWithVisualFormat:@"|[sub]|"
+                                                   options:kNilOptions
+                                                   metrics:nil
+                                                     views:@{ @"sub" : subview }];
+}
+
+
++ (NSArray *)constraintsThatFillVerticallyWithSubview:(UIView *)subview
+{
+    return [NSLayoutConstraint constraintsWithVisualFormat:@"V:|[sub]|"
+                                                   options:kNilOptions
+                                                   metrics:nil
+                                                     views:@{ @"sub" : subview }];
+}
+
+
++ (NSArray *)constraintsThatFillWithSubview:(UIView *)subview
+{
+    NSMutableArray *array = [NSMutableArray array];
+    [array addObjectsFromArray:[self constraintsThatFillHorizontallyWithSubview:subview]];
+    [array addObjectsFromArray:[self constraintsThatFillVerticallyWithSubview:subview]];
+    return [array copy];
+}
+
+
++ (NSArray *)constraintsThatCenterSubview:(UIView *)subview
+{
+    NSLayoutConstraint *horizontal = [self constraintToHorizontallyCenterSubview:subview];
+    NSLayoutConstraint *vertical = [self constraintToVerticallyCenterSubview:subview];
+    return @[ horizontal, vertical ];
+}
+
+
++ (NSLayoutConstraint *)constraintAligningView:(UIView *)view withOtherView:(UIView *)otherView onAttribute:(NSLayoutAttribute)attribute
+{
+    return [NSLayoutConstraint constraintWithItem:view
+                                        attribute:attribute
+                                        relatedBy:NSLayoutRelationEqual
+                                           toItem:otherView
+                                        attribute:attribute
+                                       multiplier:1.0
+                                         constant:0.0];
+}
+
+
++ (NSLayoutConstraint *)constraintAligningSubview:(UIView *)subview onAttribute:(NSLayoutAttribute)attribute
+{
+    return [self constraintAligningView:subview withOtherView:subview.superview onAttribute:attribute];
+}
+
+
++ (NSLayoutConstraint *)constraintToHorizontallyCenterSubview:(UIView *)subview
+{
+    return [self constraintAligningSubview:subview onAttribute:NSLayoutAttributeCenterX];
+}
+
+
++ (NSLayoutConstraint *)constraintToVerticallyCenterSubview:(UIView *)subview
+{
+    return [self constraintAligningSubview:subview onAttribute:NSLayoutAttributeCenterY];
+}
+
 @end
